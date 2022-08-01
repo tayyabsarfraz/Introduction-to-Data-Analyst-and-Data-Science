a9c7e9c7429d1773f5ff1b229b1f54714b5cb171
bsef19m521 <bsef19m521@pucit.edu.pk>
2022-08-01 20:51:16 +0500

    Module no 01 - Introduction to Python Programming

---
+++ Module no 01- Python Programming/Lecture # 15/module-files/pathissue/ehtishammodule.py
@@ -0,0 +1,1 @@
+print("This module is located in : /home/dell/Data/Introduction to data analyst and data science for begineers/Module 1(Introduction to Python)/Lecture # 15/module-files/pathissue/ directory")
